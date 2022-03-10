131a74970f14b6f1eed073f86edb8b991806ed58
mosi <99150661+mosi-sol@users.noreply.github.com>
2022-03-10 18:59:33 +0330

    Create 1-erc20-normal.sol

---
+++ erc20 standard token/1-erc20-normal.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Standard is ERC20 {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+}
